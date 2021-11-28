858b922e3c97401091d82cf3363dd029e84721b0
Senna46 <29295263+Senna46@users.noreply.github.com>
2021-11-29 02:55:21 +0900

    fix: view側の変更

--- projects/main/src/app/views/home/home.module.ts
+++ projects/main/src/app/views/home/home.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material.module';
 import { HomeComponent } from './home.component';
-
-
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, RouterModule, MaterialModule],
+  exports: [HomeComponent],
 })
-export class HomeModule { }
+export class HomeModule {}
