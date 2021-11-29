a7ccab14a656d41737878f1303803f1cc131ed20
Senna46 <29295263+Senna46@users.noreply.github.com>
2021-11-29 15:00:31 +0900

    feat: firebase deploy

--- projects/main/src/app/app.module.ts
+++ projects/main/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppHomeComponent } from './pages/home/home.component';
 import { reducers, metaReducers } from './reducers';
-import { HomeModule } from './views/home/home.module';
 import { MaterialModule } from './views/material.module';
 import { ViewModule } from './views/view.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
@@ -28,6 +27,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     RouterModule,
     MaterialModule,
     ViewModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent],
