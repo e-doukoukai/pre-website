0d7cf17c367dc60f91880aa31f4dd1f14a8d5cc6
Senna46 <29295263+Senna46@users.noreply.github.com>
2021-12-11 20:14:22 +0900

    feat: ui and printer

--- projects/main/src/app/views/accounts/signin/signin.module.ts
+++ projects/main/src/app/views/accounts/signin/signin.module.ts
@@ -3,10 +3,11 @@ import { SigninComponent } from './signin.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SigninComponent],
-  imports: [CommonModule, MaterialModule, FormsModule],
+  imports: [CommonModule, RouterModule, MaterialModule, FormsModule],
   exports: [SigninComponent],
 })
 export class SigninModule {}
