cd9a4209627bc8da7eae7f38edc24af335760b76
Senna46 <29295263+Senna46@users.noreply.github.com>
2021-12-12 06:03:20 +0900

    fix: import

--- projects/main/src/app/views/contact/contact.module.ts
+++ projects/main/src/app/views/contact/contact.module.ts
@@ -2,10 +2,11 @@ import { MaterialModule } from '../material.module';
 import { ContactComponent } from './contact.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ContactComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, RouterModule, MaterialModule],
   exports: [ContactComponent],
 })
 export class ContactModule {}
