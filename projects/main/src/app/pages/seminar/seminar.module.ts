d566c8c3e3373cd54362ef77b60706934f666ecd
KokiKumagai <stop.at.nine.k.bear53@gmail.com>
2022-10-21 00:52:59 +0900

    fix: add msg

--- projects/main/src/app/pages/seminar/seminar.module.ts
+++ projects/main/src/app/pages/seminar/seminar.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { SeminarRoutingModule } from './seminar-routing.module';
+import { CommonModule } from '@angular/common';
 import { AppSeminarComponent } from './seminar.component';
 import { SeminarModule } from '../../views/seminar/seminar.module';
 
