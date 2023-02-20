d4087ee3e7a7984b3c1cb4640b671ada2fe6b765
Ryo0731 <95175659+Ryo0731@users.noreply.github.com>
2023-02-21 01:43:47 +0900

    Revert "feat: test2022"
    This reverts commit ffa960f2b0fe16028a146d82687598934f43ebb0.

--- projects/main/src/app/pages/tests/tests.module.ts
+++ projects/main/src/app/pages/tests/tests.module.ts
@@ -1,14 +1,13 @@
-import { Tests2022Module } from '../../views/tests/tests2022/tests2022.module';
 import { TestsModule } from '../../views/tests/tests.module';
-import { AppTest2022Component } from './tests2022/test2022/test2022.component';
-import { AppTests2022Component } from './tests2022/tests2022.component';
 import { TestsRoutingModule } from './tests-routing.module';
 import { AppTestsComponent } from './tests.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { AppTests2022Component } from './tests2022/tests2022.component';
+import { Tests2022Module } from "../../views/tests/tests2022/tests2022.module";
 
 @NgModule({
-  declarations: [AppTestsComponent, AppTests2022Component, AppTest2022Component],
-  imports: [CommonModule, TestsRoutingModule, TestsModule, Tests2022Module],
+    declarations: [AppTestsComponent, AppTests2022Component],
+    imports: [CommonModule, TestsRoutingModule, TestsModule, Tests2022Module]
 })
 export class AppTestsModule {}
--- projects/main/src/app/views/tests/tests2022/tests2022.module.ts
+++ projects/main/src/app/views/tests/tests2022/tests2022.module.ts
@@ -1,12 +1,18 @@
-import { MaterialModule } from '../../material.module';
-import { Tests2022Component } from './tests2022.component';
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Tests2022Component } from './tests2022.component';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from '../../material.module';
+
+
 
 @NgModule({
-  declarations: [Tests2022Component],
-  imports: [CommonModule, RouterModule, MaterialModule],
-  exports: [Tests2022Component],
+  declarations: [
+    Tests2022Component
+  ],
+  imports: [
+    CommonModule, RouterModule, MaterialModule
+  ],
+  exports:[Tests2022Component]
 })
-export class Tests2022Module {}
+export class Tests2022Module { }
