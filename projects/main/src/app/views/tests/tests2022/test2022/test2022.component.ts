ffa960f2b0fe16028a146d82687598934f43ebb0
Ryo0731 <95175659+Ryo0731@users.noreply.github.com>
2023-02-21 01:21:08 +0900

    feat: test2022

---
+++ projects/main/src/app/views/tests/tests2022/test2022/test2022.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'view-test2022',
+  templateUrl: './test2022.component.html',
+  styleUrls: ['./test2022.component.css'],
+})
+export class Test2022Component implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
